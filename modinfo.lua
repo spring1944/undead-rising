84f764083a777d5bb74179e2aceeacd000e3451a
neddie_ <neddie_@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-03-04 00:10:11 +0000

    * Zombie Modinfo Switch

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+local modinfo = {
+		name					= "Spring: 1944 Zombies",
+		shortName				= "S44",
+		game					= "Spring 1944",
+		shortGame				= "S44",
+		mutator					= "Official",
+		description				= "Epic World War II RTS",
+		url						= "http://www.spring1944.com",
+		modtype					= "1",
+		numdependencies			= "1",
+		depend0					= "S44Pre-M_v170.sd7",
+}
+
+return modinfo
